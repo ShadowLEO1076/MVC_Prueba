bb7ab47833fcc70c31fc7b6d752addc522227b6a
MateoVasquezUisrael <e1752779908@uisrael.edu.ec>
2025-10-29 10:00:14 -0500

    Se terminó user

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,6 +3,9 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 router.get('/',  userController.getUsers);
-
+router.post('/save', userController.saveUser);
+router.get("/:id",userController.getUserById);
+router.put("/:id", userController.updateUser);
+router.delete("/:id",userController.deleteUser);
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log("Hello, World!");
 const express = require('express');
 const userRouter = require('./Routes/userRoutes');
 const app = express();
